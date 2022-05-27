878d94ae11dd35ae2ee8bae7c6839d976f9ebe32
pmeinhardt <mnhrdt@gmail.com>
2022-05-27 12:00:52 +0200

    Restrict Jest to "src" directory

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,7 @@ export default {
   },
   globalSetup: "./dev/jest/global-setup.ts",
   preset: "ts-jest/presets/default",
+  roots: ["<rootDir>/src/"],
   setupFilesAfterEnv: ["./dev/jest/setup.ts"],
   testEnvironment: "jest-environment-jsdom",
   transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
